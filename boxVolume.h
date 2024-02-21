b06e6f6efe5cc18513c231b7f35ffb39062d5039
Prasadraogorle <159033802+Prasadraogorle@users.noreply.github.com>
2024-02-21 17:18:35 +0530

    Create boxVolume.h

---
+++ boxVolume.h
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+void boxVolume(float,float,float);
+void boxVolume(float length,float width,float height)
+{
+    cout<<"The area of given box is : "<<length*width*height<<endl;
+}
